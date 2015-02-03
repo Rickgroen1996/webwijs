bc36b10804faa3480f1b056b5d13f60ef4caa960
jairzijp <jairzijp123@gmail.com>
2015-02-03 13:54:17 +0100

    template + materialize

--- html5blank-stable/header.php
+++ html5blank-stable/header.php
@@ -7,16 +7,15 @@
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
+		<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/materialize.min.css"  media="screen,projection"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
-
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/materialize.min.js"></script>
 		<?php wp_head(); ?>
 		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
+       	conditionizr.config({
             assets: '<?php echo get_template_directory_uri(); ?>',
             tests: {}
         });
@@ -38,13 +37,11 @@
 							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
+					
 					<nav class="nav" role="navigation">
 						<?php html5blank_nav(); ?>
 					</nav>
-					<!-- /nav -->
+					
 
 			</header>
-			<!-- /header -->
+			
