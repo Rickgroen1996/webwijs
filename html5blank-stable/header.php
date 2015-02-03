08fb4e1b6bfd67ba6e4d48de122690da840a5491
jairzijp <jairzijp123@gmail.com>
2015-02-03 14:39:52 +0100

    kleine veranderingen

--- html5blank-stable/header.php
+++ html5blank-stable/header.php
@@ -34,14 +34,13 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					
+					<nav class="nav teal lighten-2" role="navigation">
 
+						<?php wp_nav_menu(); ?>
+					</nav>
 			</header>
 			
