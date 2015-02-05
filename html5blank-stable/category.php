4433a33fca3d1c075f3f17f124ed49d8ca08db58
jairzijp <jairzijp123@gmail.com>
2015-02-05 09:48:46 +0100

    Styling posts en categorien

--- html5blank-stable/category.php
+++ html5blank-stable/category.php
@@ -9,11 +9,11 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Categorie ', 'html5blank' ); single_cat_title(); ?></h1>
+			<!--<h1><?php _e( 'Categorie ', 'html5blank' ); single_cat_title(); ?></h1>-->
 
 			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
+			<p><?php the_content(); ?></p>
+			
 
 		</section>
 		<!-- /section -->
